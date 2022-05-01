dd22a132e68e47ba746f8ec161b87edcc73e2540
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2022-05-01 11:32:12 -0400

    extract code into homeview and searchview

---
+++ starter/src/SearchView.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SearchView = () => {
+  return (
+    <div className='search-books'>
+      <div className='search-books-bar'>
+        <a className='close-search' onClick={() => {}}>
+          Close
+        </a>
+        <div className='search-books-input-wrapper'>
+          <input type='text' placeholder='Search by title, author, or ISBN' />
+        </div>
+      </div>
+      <div className='search-books-results'>
+        <ol className='books-grid'></ol>
+      </div>
+    </div>
+  );
+};
+
+export default SearchView;
